92475a522de199700f09afbcc6746ce2081d4779
Matthew Trentacoste <web+git@matttrent.com>
2017-08-17 23:39:06 -0700

    cache-buster suffix

--- metalsmith.js
+++ metalsmith.js
@@ -83,7 +83,8 @@ var M = metalsmith(__dirname)
   current_date:         moment().format('DD MMMM YYYY'),
   current_month:        moment().format('MMMM YYYY'),
   current_year:         moment().year(),
-  years_working:        moment().year() - moment("2004", "YYYY").year() + 1
+  years_working:        moment().year() - moment("2004", "YYYY").year() + 1,
+  cache_suffix:         'v=' + String(Math.round( Math.random() * 1e7 )),
 }))
 
 // ability to store posts as drafts by placing the following front-matter:
