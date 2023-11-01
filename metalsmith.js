d9b32fd72bf12d86954bfacec04ff4db43178a17
Matthew Trentacoste <web+git@matttrent.com>
2023-11-01 09:02:03 -0600

    Lots of changes to metalsmith + config
    Updating metalsmith package + major plugins
    Adding a notes collection
    Reordering plugins to support correctly using collections in templates

--- metalsmith.js
+++ metalsmith.js
@@ -6,28 +6,33 @@ var metalsmith  = require('metalsmith');
 
 //                https://github.com/aymericbeaumet/metalsmith-define
 var define      = require('metalsmith-define');
-//                https://github.com/ericgj/metalsmith-branch
-var branch      = require('metalsmith-branch');
-//                https://github.com/segmentio/metalsmith-collections
-// var collections = require('metalsmith-collections');
+// //                https://github.com/ericgj/metalsmith-branch
+// var branch      = require('metalsmith-branch');
+//                https://github.com/metalsmith/collections
+var collections = require('@metalsmith/collections');
 //                https://github.com/attentif/metalsmith-markdown-remarkable
 var remarkable  = require('metalsmith-markdown-remarkable');
 //                https://github.com/algenon/metalsmith-typography
 var typography  = require('metalsmith-typography');
-//                https://github.com/segmentio/metalsmith-permalinks
-var permalinks  = require('metalsmith-permalinks');
-//                https://github.com/segmentio/metalsmith-drafts
-var drafts      = require('metalsmith-drafts');
-//                https://github.com/superwolff/metalsmith-layouts
-var layouts     = require('metalsmith-layouts');
-//                https://github.com/superwolff/metalsmith-in-place
-var inplace     = require('metalsmith-in-place');
+//                https://github.com/metalsmith/permalinks
+var permalinks  = require('@metalsmith/permalinks');
+//                https://github.com/metalsmith/drafts
+var drafts      = require('@metalsmith/drafts');
+//                https://github.com/metalsmith/layouts
+var layouts     = require('@metalsmith/layouts');
+//                https://github.com/metalsmith/in-place
+var inplace     = require('@metalsmith/in-place');
 //                https://github.com/treygriffith/metalsmith-assets
 var assets      = require('metalsmith-assets');
 //                https://github.com/timdp/metalsmith-discover-partials
 var discoverPartials = require('metalsmith-discover-partials');
 //                https://github.com/alex-ketch/metalsmith-renamer
 var renamer     = require('metalsmith-renamer');
+
+// var debugUI     = require('metalsmith-debug-ui');
+
+
+// ----------------------------------------------------------------------------
 
 
 // register handlebars layout helper
@@ -57,94 +62,146 @@ handlebars.registerHelper('marginnote', function(name, options) {
   );
 });
 
-// ----
-
-// init the metalsmith object
-var M = metalsmith(__dirname)
-
-// set global metadata 
-.metadata({
-  site: {
-    title: 'matt trent',
-    url: 'https://matttrent.com'
-  }
+handlebars.registerHelper('filedate', function(file, format="YYYY MMM") {
+    return moment(file.date).format(format);
 })
 
-// the source directory from which to parse content
-.source('./src')
 
-// the destination to which deliver processed results
-.destination('./build')
+// ----------------------------------------------------------------------------
 
-// define our defines
-// available in handlbars in the form {{ attachments_prefix }}
-.use(define({
-  attachments_prefix:   '/attachments',
-  _:                    require('underscore'),
-  current_date:         moment().format('DD MMMM YYYY'),
-  current_month:        moment().format('MMMM YYYY'),
-  current_year:         moment().year(),
-  cache_suffix:         'v=' + String(Math.round( Math.random() * 1e7 )),
-}))
 
-// ability to store posts as drafts by placing the following front-matter:
-// draft: true
-.use(drafts())
+// init the metalsmith object
+var M = metalsmith(__dirname);
 
-// rename all the .html and .md files to have a second .hbs suffix
-// this way both inplace and layouts know to apply the handlebars transformer
-// to the file
-.use(renamer({
-  filesToRename: {
-    pattern: '**/*\+(html|md)',
-    rename: function (name) {
-      return name + '.hbs';
+// debugUI.patch(M)
+
+M
+  // ==== metalsmith build configuration ======================================
+  // the source directory from which to parse content
+  .source('./src')
+
+  // the destination to which deliver processed results
+  .destination('./build')
+
+  // clean destination before
+  .clean(true)                
+
+  // pass NODE_ENV & other environment variables
+  .env({
+    DEBUG: process.env.DEBUG,
+    NODE_ENV: process.env.NODE_ENV
+  })      
+
+  // ==== site content definitions ============================================
+  // define our defines
+  // available in handlbars in the form {{ attachments_prefix }}
+  .use(define({
+    attachments_prefix:   '/attachments',
+    _:                    require('underscore'),
+    current_date:         moment().format('DD MMMM YYYY'),
+    current_month:        moment().format('MMMM YYYY'),
+    current_year:         moment().year(),
+    cache_suffix:         'v=' + String(Math.round( Math.random() * 1e7 )),
+  }))
+
+  // // set global metadata
+  // // TODO: replace use(define()) with this
+  // .metadata({
+  //   site: {
+  //     title: 'matt trent',
+  //     url: 'https://matttrent.com',
+  //     attachments_prefix: '/attachments',
+  //     current_date: moment().format('DD MMMM YYYY'),
+  //     current_month: moment().format('MMMM YYYY'),
+  //     current_year: moment().year(),
+  //     cache_suffix: 'v=' + String(Math.round(Math.random() * 1e7)),
+  //   }
+  // })
+
+  // ability to store posts as drafts by placing the following front-matter:
+  // draft: true
+  .use(drafts())
+
+  // create a collection for notes
+  .use(collections({
+    notes: {
+      pattern: 'notes/**/*\+(html|md)',
+      sortBy: 'date',
+      reverse: true,
     }
-  },
-}))
+  }))
 
-// search for partials in the same layouts directory
-.use(discoverPartials({
-  directory: 'layouts',
-  pattern: /\.hbs$/
-}))
+  // ==== render interal contents of files ====================================
+  // rename all the .html and .md files to have a second .hbs suffix
+  // this way both inplace and layouts know to apply the handlebars transformer
+  // to the file
+  .use(renamer({
+    filesToRename: {
+      pattern: '**/*\+(html|md)',
+      rename: function (name) {
+        return name + '.hbs';
+      }
+    },
+  }))
 
-// in-place templating to let handlebars access metadata, defines and 
-// front-matter when processing templates
-.use(inplace())
+  // search for partials in the same layouts directory
+  // TODO: am I using this?
+  .use(discoverPartials({
+    directory: 'layouts',
+    pattern: /\.hbs$/
+  }))
 
-// process markdown files
-.use(remarkable({
-  html: true,
-  typographer: true,
-}))
+  // in-place templating to let handlebars access metadata, defines and 
+  // front-matter when processing templates
+  .use(inplace({
+    transform: 'handlebars',
+  }))
 
-// use richtypo.js when processing
-.use(typography({
-  lang: "en"
-}))
+  // process markdown files
+  .use(remarkable({
+    html: true,
+    typographer: true,
+  }))
 
-// use handlebars as our templating engine
-// handlebars also processes partial templates, which are stored in the same directory
-.use(layouts({
-  directory: 'layouts'
-}))
+  // use richtypo.js when processing
+  .use(typography({
+    lang: "en"
+  }))
 
-// generate a "permalink", trasforming:
-// /about.html -> /about/index.html so /about will work as a url
-.use(permalinks())
+  // ==== permalink / move to final location ==================================
+  // generate a "permalink", trasforming:
+  // /about.html -> /about/index.html so /about will work as a url
+  // .use(permalinks())
+  .use(permalinks(
+    {
+      relative: false,
+      linksets: [
+        {
+          match: { collection: 'notes' },
+          date: 'YYYY',
+          pattern: 'notes/:date/:slug',
+        }
+      ]
+    }
+  ))
 
-// copy static ./assets and ./attachments to the destination directory
-.use(assets({
-  source:       "./assets",
-  destination:  "./assets"
-}))
-.use(assets({
-  source:       "./attachments",
-  destination:  "./attachments"
-}))
+  // ==== render full file contents with layout ===============================
+  // use handlebars as our templating engine
+  .use(layouts({
+    directory: 'layouts'
+  }))
 
-;
+  // ==== add assets to build =================================================
+  // copy static ./assets and ./attachments to the destination directory
+  .use(assets({
+    source:       "./assets",
+    destination:  "./assets"
+  }))
+  .use(assets({
+    source:       "./attachments",
+    destination:  "./attachments"
+  }))
+  ;
 
 // some export stuff to run imported as a local dev server / on heroku
 if (module.parent) {
